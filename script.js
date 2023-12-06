eca97e2390bc65272a9bd96fdc68c23cbf1441a7
dalexxi <89869386+dalexxi@users.noreply.github.com>
2023-12-06 14:55:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    loadData();
-});
-
-function loadData() {
-    fetch('data.json')
-        .then(response => response.json())
-        .then(data => loadContent(data))
-        .catch(error => console.error('Error loading data:', error));
-}
-
-function loadContent(data) {
-    var html = '';
-    data.forEach(category => {
-        html += '<div class="category"><h1>' + category.title + '</h1>';
-        category.terms.forEach(term => {
-            html += '<div class="accordion-label"><h2>' + term.termTitle + '</h2></div>';
-            html += '<div class="accordion">';
-            term.units.forEach(unit => {
-                html += '<div class="unit-label"><h3>' + unit.unitTitle + '</h3></div>';
-                html += '<div class="unit-accordion">';
-                unit.lessons.forEach(lesson => {
-                    if(lesson.contents && lesson.contents.length > 0) {
-                        html += '<div class="lesson-label"><h4>' + lesson.lessonTitle + '</h4></div>';
-                        html += '<div class="lesson-accordion">';
-                        lesson.contents.forEach(subLesson => {
-                            html += '<div class="sublesson-label"><p>' + subLesson.subLessonTitle + '</p></div>';
-                            html += '<div class="sublesson-accordion">';
-                            // Add content for sub-lesson here
-                            html += '</div>'; // Close sublesson-accordion
-                        });
-                        html += '</div>'; // Close lesson-accordion
-                    } else {
-                        html += '<p><a href="' + lesson.link + '">' + lesson.lessonTitle + '</a></p>';
-                    }
-                });
-                html += '</div>'; // Close unit-accordion
-            });
-            html += '</div>'; // Close accordion
-        });
-        html += '</div>'; // Close category
-    });
-
-    document.getElementById("content").innerHTML = html;
-    attachAccordionEvents();
-}
-
-function attachAccordionEvents() {
+    loadContent();
     document.getElementById("content").addEventListener("click", function(e) {
         var accordionLabel = e.target.classList.contains("accordion-label") 
                              ? e.target 
@@ -76,6 +30,78 @@ function attachAccordionEvents() {
             toggleSubLessonAccordion(subLessonLabel.nextElementSibling);
         }
     });
+});
+
+function loadContent() {
+    var data = [
+        {
+            title: "Secondary 3",
+            terms: [
+                {
+                    termTitle: "Term 1",
+                    units: [
+                        {
+                            unitTitle: "Unit 1 & 2",
+                            lessons: [
+                                    { 
+                                        lessonTitle: "Lesson 1",
+                                        contents: [
+                                            {subLessonTitle: "Lesson 1A", link: "#link1A"}
+                                        ]
+                                    },
+
+                                    { lessonTitle: "Lesson 2", link: "#link2"},
+                                    { 
+                                        lessonTitle: "Lesson 3",
+                                        contents: [
+                                            {subLessonTitle: "Lesson 3A", link: "#link3A"}
+                                        ]
+                                    }
+                            ]
+                        }
+                        // More units and lessons
+                    ]
+                }
+                // More terms
+            ]
+        }
+        // More categories like Secondary 4
+    ];
+
+    var html = '';
+    data.forEach(category => {
+        html += '<div class="category"><h1>' + category.title + '</h1>';
+
+        category.terms.forEach(term => {
+            html += '<div class = "accordion-label"><h2>' + term.termTitle + '</h2></div>';
+            html += '<div class = "accordion">';
+
+            term.units.forEach(unit => {
+                html += '<div class="unit-label"><h3>' + unit.unitTitle + '</h3></div>';
+                html += '<div class="unit-accordion">';
+
+                unit.lessons.forEach(lesson => {
+                    if(lesson.contents && lesson.contents.length >0){
+                        html += '<div class="lesson-label"><h4>' + lesson.lessonTitle + '</h4></div>';
+                        html += '<div class="lesson-accordion">';
+                            
+                        lesson.contents.forEach(subLesson => {
+                            html += '<h5><a href="' + subLesson.link + '">' + subLesson.subLessonTitle + '</h5></p>';
+                        });
+                        html += '</div>'; //lesson - accordion
+                    }
+                    else{
+                        html += '<h4><a href="' + lesson.link + '">' + lesson.lessonTitle + '</h4></p>';
+                    }
+                });
+                html += '</div>'; //unit - accordion
+            });
+            html += '</div>'; //close accordion
+        });
+        html += '</div>'; //close category
+    });
+
+    document.getElementById("content").innerHTML = html;
 }
 
 function toggleAccordion(accordion) {
