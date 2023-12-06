9e0490a55d4e3f0b64f9ac3d2bcd729146eff01c
dalexxi <89869386+dalexxi@users.noreply.github.com>
2023-12-06 16:05:10 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,43 @@
-
+var data = [
+        {
+            title: "Secondary 3",
+            terms: [
+                {
+                    termTitle: "Term 2",
+                    units: [
+                        {
+                            unitTitle: "Unit 1 & 2",
+                            lessons: [
+                                { lessonTitle: "Lesson 1 - Communication with Other ICT Users Using Email", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S3/term2/unit1-2/S3_T2_U1L1.html"},
+                                { lessonTitle: "Lesson 2 - Effective Use of the Internet", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S3/term2/unit1-2/S3_T2_U1L2.html"},
+                                { lessonTitle: "Lesson 3 - Audience Appreciation ", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S3/term2/unit1-2/S3_T2_U1L3.html"}
+                            ]
+                        }
+                        // More units and lessons
+                    ]
+                }
+                // More terms
+            ]
+        },
+        // More categories like Secondary 4
+        {
+            title: "Secondary 4",
+            terms: [
+                {
+                    termTitle: "Term 2",
+                    units: [
+                        {
+                            unitTitle: "Unit 1 & 2",
+                            lessons: [
+                                { lessonTitle: "Lesson 1 - Communication with Other ICT Users Using Email", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S3/term2/unit1-2/S3_T2_U1L1.html"},
+                                { lessonTitle: "Lesson 2 - Effective Use of the Internet", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S3/term2/unit1-2/S3_T2_U1L2.html"},
+                                { lessonTitle: "Lesson 3 - Audience Appreciation ", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S3/term2/unit1-2/S3_T2_U1L3.html"}
+                            ]
+                        }
+                        // More units and lessons
+                    ]
+                }
+                // More terms
+            ]
+        }
+    ];
