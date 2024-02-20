6bd327dcc571f6dae4fb82b77a90605587c34b91
dalexxi <89869386+dalexxi@users.noreply.github.com>
2024-02-20 11:38:26 +0800

    Update data.js

--- data.js
+++ data.js
@@ -8,9 +8,16 @@ var data = [{
                                 [{ lessonTitle: "Lesson 1 - Communication with Other ICT Users Using Email", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S3/term2/unit1-2/S3_T2_U1L1.html"},
                                 { lessonTitle: "Lesson 2 - Effective Use of the Internet", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S3/term2/unit1-2/S3_T2_U1L2.html"},
                                 { lessonTitle: "Lesson 3 - Audience Appreciation ", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S3/term2/unit1-2/S3_T2_U1L3.html"}
-                                ]}// More units and lessons
+                                ]},
+                            {
+                             unitTitle: "Unit 5 & 6",
+                             lessons:
+                                [{ lessonTitle: "Lesson 1 - Images", link: "h5p-standalone-1.3.x/workspace/S3/term2/unit5-6/S3_U5-6L1_Images.html"}]
+                            }
+                            // More units and lessons
                             ]}// More terms
                     ]},
+            {}
         // More categories like Secondary 4
         {
             title: "Secondary 4",
