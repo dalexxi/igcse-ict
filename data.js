e643dd5d79bf2d1940b033112980f40be5fecb11
dalexxi <89869386+dalexxi@users.noreply.github.com>
2025-05-29 10:53:15 +0800

    Update data.js

--- data.js
+++ data.js
@@ -33,7 +33,10 @@ var data = [{
                                 { lessonTitle: "Lesson 3 - Audience Appreciation ", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S3/term2/unit5-6/ICT_S3_U5-6L2_Layout.html"},
                                 { lessonTitle: "Lesson 4 - Escape Room 1 ", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S4/term2/unit3-4/ICT Escape Room Station 1.html"},
                                 { lessonTitle: "Lesson 5 - Escape Room 2 ", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S4/term2/unit3-4/ICT Escape Room Station 2.html"},
-                                { lessonTitle: "Lesson 6 - Escape Room 3 ", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S4/term2/unit3-4/ICT Escape Room Station 3.html"},                                        
+                                { lessonTitle: "Lesson 6 - Escape Room 3 ", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S4/term2/unit3-4/ICT Escape Room Station 3.html"},
+                                { lessonTitle: "Lesson 7 - Escape Room 4 ", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S4/term2/unit3-4/ICT Escape Room Station 4.html"},
+                                { lessonTitle: "Lesson 8 - Escape Room 5 ", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S4/term2/unit3-4/ICT Escape Room Station 5.html"},
+                                { lessonTitle: "Lesson 9 - Escape Room 6 ", link: "https://dalexxi.github.io/igcse-ict/h5p-standalone-1.3.x/workspace/S4/term2/unit3-4/ICT Escape Room Station Final Station.html"},
                                 ]
                     }// More units and lessons
                 ]}// More terms
